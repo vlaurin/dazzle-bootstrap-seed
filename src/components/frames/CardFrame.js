6b592533d3b7ac0dafcd6a961b0ff99a2b795c23
Valentin Laurin <me@vlaurin.eu>
2018-03-03 21:42:47 +0000

    Make it dark

--- src/components/frames/CardFrame.js
+++ src/components/frames/CardFrame.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const CardFrame = ({children, title}) => (
-    <div className="card">
+    <div className="card bg-white text-dark">
         <div className="card-body">
             <h5 className="card-title">{title}</h5>
             {children}
