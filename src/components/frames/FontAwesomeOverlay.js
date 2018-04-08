32bb2e98525a4bfbc1d824a22b54ad2dc71b3b75
Valentin Laurin <me@vlaurin.eu>
2018-04-08 18:48:05 +0100

    Add Fontawesome overlay component

---
+++ src/components/frames/FontAwesomeOverlay.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import FontAwesomeIcon from '@fortawesome/react-fontawesome';
+
+const style = {
+    fontSize: '10vw',
+    color: 'lightGray',
+    opacity: '.3',
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+};
+
+const FontAwesomeOverlay = ({icon}) => (
+    <div style={style}>
+        <FontAwesomeIcon icon={icon} />
+    </div>
+);
+
+export default FontAwesomeOverlay;
