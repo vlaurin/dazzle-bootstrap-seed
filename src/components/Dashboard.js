d03db37ad8db990b94040937f906b5bd5fee6852
Valentin Laurin <me@vlaurin.eu>
2018-04-20 07:48:58 +0100

    Update sample dashboard to use all theme colours

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -3,6 +3,11 @@ import DazzleDashboard from 'react-dazzle';
 import faCoffee from '@fortawesome/fontawesome-free-solid/faCoffee';
 import faClock from '@fortawesome/fontawesome-free-solid/faClock';
 import faAnchor from '@fortawesome/fontawesome-free-solid/faAnchor';
+import faBatteryFull from '@fortawesome/fontawesome-free-solid/faBatteryFull';
+import faCogs from '@fortawesome/fontawesome-free-solid/faCogs';
+import faCodeBranch from '@fortawesome/fontawesome-free-solid/faCodeBranch';
+import faTrain from '@fortawesome/fontawesome-free-solid/faTrain';
+import faBell from '@fortawesome/fontawesome-free-solid/faBell';
 import HelloWorld from './widgets/HelloWorld';
 import Container from './Container';
 import SmashingFrame from './frames/SmashingFrame';
@@ -29,7 +34,7 @@ class Dashboard extends Component {
                     type: HelloWorld,
                     props: {
                         title: 'Widget #2',
-                        subtitle: 'This is some rather long content to make widgets of different heights.',
+                        subtitle: 'Hello World',
                     },
                     frameSettings: {
                         icon: faClock,
@@ -47,6 +52,61 @@ class Dashboard extends Component {
                         colour: 'yellow',
                     }
                 },
+                Widget4: {
+                    type: HelloWorld,
+                    props: {
+                        title: 'Widget #4',
+                        subtitle: 'Hello World',
+                    },
+                    frameSettings: {
+                        icon: faBatteryFull,
+                        colour: 'green',
+                    }
+                },
+                Widget5: {
+                    type: HelloWorld,
+                    props: {
+                        title: 'Widget #5',
+                        subtitle: 'Hello World',
+                    },
+                    frameSettings: {
+                        icon: faCogs,
+                        colour: 'aqua',
+                    }
+                },
+                Widget6: {
+                    type: HelloWorld,
+                    props: {
+                        title: 'Widget #6',
+                        subtitle: 'Hello World',
+                    },
+                    frameSettings: {
+                        icon: faCodeBranch,
+                        colour: 'blue',
+                    }
+                },
+                Widget7: {
+                    type: HelloWorld,
+                    props: {
+                        title: 'Widget #7',
+                        subtitle: 'Hello World',
+                    },
+                    frameSettings: {
+                        icon: faTrain,
+                        colour: 'purple',
+                    }
+                },
+                Widget8: {
+                    type: HelloWorld,
+                    props: {
+                        title: 'Widget #8',
+                        subtitle: 'Hello World',
+                    },
+                    frameSettings: {
+                        icon: faBell,
+                        colour: 'background',
+                    }
+                },
             },
             layout: {
                 rows: [
@@ -54,15 +114,19 @@ class Dashboard extends Component {
                         columns: [
                             {
                                 className: 'col-md',
-                                widgets: [{key: 'Widget1'}, {key: 'Widget3'}],
+                                widgets: [{key: 'Widget1'}, {key: 'Widget5'}],
                             },
                             {
                                 className: 'col-md',
-                                widgets: [{key: 'Widget2'}],
+                                widgets: [{key: 'Widget2'}, {key: 'Widget6'}],
                             },
                             {
                                 className: 'col-md',
-                                widgets: [{key: 'Widget3'}],
+                                widgets: [{key: 'Widget3'}, {key: 'Widget7'}],
+                            },
+                            {
+                                className: 'col-md',
+                                widgets: [{key: 'Widget4'}, {key: 'Widget8'}],
                             },
                         ],
                     },
